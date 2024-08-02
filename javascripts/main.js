0637d6c389c2a7c8f2bd96c03cf1308bcc535d24
Snooproxy Team <173563672+snooproxy@users.noreply.github.com>
2024-08-02 20:12:49 +0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -24,124 +24,27 @@ function setParameterByName(key, value) {
   window.history.replaceState({}, "", baseUrl + params);
 }
 
+// define the dial
 var dial = d3.select(".dial")
     .attr("width", "38mm")
     .attr("height", "38mm");
 
+// define the dial color
 dial.append("rect")
     .attr("x","0mm")
     .attr("y","0mm")
     .attr("width","38mm")
-    .attr("height","15.7mm")
+    .attr("height","38mm")
     .attr("fill","#82836d");
 
-dial.append("rect")
-    .attr("x","0mm")
-    .attr("y","15.7mm")
-    .attr("width","38mm")
-    .attr("height","22.2mm")
-    .attr("fill","#fff");
-
-dial.append("line")
-    .attr("x1", "0mm")
-    .attr("x2", "38mm")
-    .attr("y1", "19.2mm")
-    .attr("y2", "19.2mm")
-    .attr("stroke", "#82836d");
-
-dial.append("line")
-    .attr("x1", "0mm")
-    .attr("x2", "38mm")
-    .attr("y1", "19.2mm")
-    .attr("y2", "19.2mm")
-    .attr("stroke", "#82836d");
-
-dial.append("line")
-    .attr("x1", "15.5mm")
-    .attr("x2", "15.5mm")
-    .attr("y1", "0mm")
-    .attr("y2", "16mm")
-    .attr("stroke", "#fff");
-
-dial.append("line")
-    .attr("x1", "15.5mm")
-    .attr("x2", "15.5mm")
-    .attr("y1", "15.7mm")
-    .attr("y2", "19.2mm")
-    .attr("stroke", "#82836d");
-
-dial.append("circle")
-    .attr("cx", "19mm")
-    .attr("cy", "19mm")
-    .attr("r", "19mm")
-    .attr("stroke", "#ccc")
-    .attr("stroke-width", "0.1mm")
-    .attr("fill", "none");
-
-dial.append("text")
-    .attr("class", "info")
-    .attr("x", "17.5mm")
-    .attr("y", "5mm")
-    .text("FIRE");
-
-dial.append("text")
-    .attr("class", "info")
-    .attr("x", "17.5mm")
-    .attr("y", "9.5mm")
-    .text("POLICE");
-
-dial.append("text")
-    .attr("class", "info")
-    .attr("x", "17.5mm")
-    .attr("y", "14mm")
-    .text("AMBULANCE");
-
-dial.append("text")
-    .attr("class", "info")
-    .attr("x", "14mm")
-    .attr("y", "9.5mm")
-    .text("DIAL")
-    .style("font-size","3.8mm")
-    .style("text-align","right")
-    .style("text-anchor","end")
-    .style("fill", "#fff");
-
-dial.append("text")
-    .attr("class", "info")
-    .attr("x", "14mm")
-    .attr("y", "14mm")
-    .text("999")
-    .style("font-size","4.0mm")
-    .style("text-align","right")
-    .style("text-anchor","end")
-    .style("letter-spacing", "0.7mm");
-
-dial.append("text")
-    .attr("class", "info")
-    .attr("x", "18.5mm")
-    .attr("y", "18.4mm")
-    .text("OPERATOR")
-    .style("text-align","center")
-    .style("text-anchor","left")
-    .style("fill", "#82836d");
-
-dial.append("text")
-    .attr("class", "info")
-    .attr("x", "11.7mm")
-    .attr("y", "18.5mm")
-    .text("100")
-    .style("font-size","3.2mm")
-    .style("text-align","center")
-    .style("text-anchor","end")
-    .style("letter-spacing", "0.3mm")
-    .style("fill", "#82836d");
-
+// add area number
 dial.append("text")
     .attr("class", "exch")
     .attr("x", "19mm")
     .attr("y", "24.1mm")
     .text(getParameterByName("e"));
 
+// add phone number
 dial.append("text")
     .attr("class", "num")
     .attr("x", "19mm")
